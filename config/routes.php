fb1206eb9d73955d5823a96d6de42cff532e1b10
ukyoda <black12172001@hotmail.co.jp>
2016-07-13 01:06:55 +0900

    更新漏れ

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,29 @@ Router::plugin(
     ['path' => '/admin-manager'],
     function ($routes) {
         $routes->fallbacks('DashedRoute');
+        $routes->connect(
+            '/',
+            ['controller' => 'Dashboard', 'action' => 'index']
+        );
+        $routes->connect(
+            '/users/:id',
+            ['controller' => 'Users', 'action' => 'view'],
+            ['id' => '\d+', 'pass' => ['id']]
+        );
+        $routes->connect(
+            '/users/:id/edit',
+            ['controller' => 'Users', 'action' => 'edit'],
+            ['id' => '\d+', 'pass' => ['id']]
+        );
+        $routes->connect(
+            '/users/:id/password',
+            ['controller' => 'Users', 'action' => 'edit-password'],
+            ['id' => '\d+', 'pass' => ['id']]
+        );
+        $routes->connect(
+            '/users/:id/delete',
+            ['controller' => 'Users', 'action' => 'delete'],
+            ['id' => '\d+', 'pass' => ['id']]
+        );
     }
 );
